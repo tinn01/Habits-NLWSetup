cc44929dd36cc791d88da3c5dc92e517d39d9c11
Alberto Guedes <121692147+tinn01@users.noreply.github.com>
2023-01-20 18:15:42 -0300

    added eventListeners and functions

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const form = document.querySelector("form");
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
-const data = {
-  run: ["01-19", "01-20", "01-21", "01-22", "01-23"],
-  water: ["01-19", "01-20", "01-21"],
-  gym: ["01-19", "01-20", "01-21"],
-};
+button.addEventListener("click", addD);
+form.addEventListener("change", save);
+
+function addD() {
+  today = new Date().toLocaleDateString().slice(0, -5);
+  dayExists = nlwSetup.dayExists(today);
+
+  if (dayExists) {
+    alert("Day already exists ❌");
+    return;
+  } else {
+    alert("Added Successfully ✔");
+  }
+
+  nlwSetup.addDay(today);
+}
+
+function save() {
+  localStorage.setItem("nlwSaveChanges", JSON.stringify(nlwSetup.data));
+}
+
+data = JSON.parse(localStorage.getItem("nlwSaveChanges")) || {};
 
 nlwSetup.setData(data);
-
 nlwSetup.load();
